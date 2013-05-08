6866a82a7ee39a2af4cd40ec578f4aed6c39b3d5
TomOne <user.email@fake.com>
2013-05-08 23:27:59 +0200

    fixed assaultcube package used program files folder as settings storage, now it's the my documents folder to work without administrative rights

--- assaultcube/tools/chocolateyInstall.ps1
+++ assaultcube/tools/chocolateyInstall.ps1
@@ -1,1 +1,12 @@
-Install-ChocolateyPackage 'assaultcube' 'exe' '/S' 'http://sourceforge.net/projects/actiongame/files/AssaultCube%20Version%201.1.0.4/AssaultCube_v1.1.0.4.exe/download'
+$packageName = "assaultcube"
+$url = "http://sourceforge.net/projects/actiongame/files/AssaultCube%20Version%201.1.0.4/AssaultCube_v1.1.0.4.exe/download"
+$zipPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\assaultcube.zip"
+$assaultcubePath = 
+if (Test-Path "$env:ProgramFiles\AssaultCube_v1.1.0.4") {
+    $unzipLocation = "$env:ProgramFiles\AssaultCube_v1.1.0.4"
+} else {
+    $unzipLocation = "${env:ProgramFiles(x86)}\AssaultCube_v1.1.0.4"
+}
+
+Install-ChocolateyPackage $packageName 'exe' '/S' $url
+Install-ChocolateyZipPackage $packageName $zipPath $unzipLocation
