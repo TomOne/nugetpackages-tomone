ccdaf8f9554eada6ebe5c02220e0c552a812beb4
Antonio David Gellida Lavara <venerable13@gmail.com>
2013-05-15 23:09:50 +0200

    Doing installable on Windows 8
    This way the installer is executed in Win7 compatibility mode if you has a Windows 8 version.

--- ext2fsd/tools/chocolateyInstall.ps1
+++ ext2fsd/tools/chocolateyInstall.ps1
@@ -1,1 +1,8 @@
-Install-ChocolateyPackage 'ext2fsd' 'exe' '/verysilent' 'http://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.51/Ext2Fsd-0.51.exe'
+$os_name = (Get-WmiObject -class Win32_OperatingSystem).Caption
+if ($os_name -like '* 8*'){
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags" -Name Layers 
+
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -propertyType String -Name "$env:temp\chocolatey\ext2fsd\ext2fsdInstall.exe" -value "~ RUNASADMIN WIN7RTM"
+}
+
+Install-ChocolateyPackage 'ext2fsd' 'exe' '' 'http://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.51/Ext2Fsd-0.51.exe'
