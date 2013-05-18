540cc67bfa97fdde3b84e817340ddb400afc2263
TomOne <user.email@fake.com>
2013-05-18 02:55:56 +0200

    added srwareiron

---
+++ srwareiron/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+$unfile = "SRWare Iron\unins000.exe"
+
+if (Test-Path "${Env:ProgramFiles(x86)}\$unfile") {
+    $unpath = "${Env:ProgramFiles(x86)}\$unfile"
+}
+else {
+    $unpath = "${Env:ProgramFiles}\$unfile"
+}
+Uninstall-ChocolateyPackage 'SRWareIron' 'exe' '/silent' "$unpath"
