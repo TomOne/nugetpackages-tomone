8408721af67aa042cb43a006c339abb19200f2fb
TomOne <user.email@fake.com>
2013-05-19 18:52:46 +0200

    added compatibility mode for Windows 8 and higher

--- ext2fsd/tools/chocolateyInstall.ps1
+++ ext2fsd/tools/chocolateyInstall.ps1
@@ -1,8 +1,18 @@
-$os_name = (Get-WmiObject -class Win32_OperatingSystem).Caption
-if ($os_name -like '* 8*'){
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags" -Name Layers 
+$packageName = "ext2fsd"
+$fileType = "exe"
+$silentArgs = "/VERYSILENT"
+$url = "http://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.51/Ext2Fsd-0.51.exe"
 
-New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -propertyType String -Name "$env:temp\chocolatey\ext2fsd\ext2fsdInstall.exe" -value "~ RUNASADMIN WIN7RTM"
+# Set compatibility mode to Windows 7 if operating system is Windows 8 or higher.
+$WindowsVersion = (Get-WmiObject -class Win32_OperatingSystem).Version
+if ($WindowsVersion -ge "6.2.9200") {
+    if (-not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags" -Name Layers 
+    }
+    New-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -propertyType String -Name "$env:temp\chocolatey\$packageName\$packageName`Install.$fileType" -value "~ WIN7RTM"
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+    # Delete compatibility mode for ext2fsd installer, because it’s not needed anymore
+    Remove-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$env:temp\chocolatey\$packageName\$packageName`Install.$fileType"
+} else {
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url
 }
-
-Install-ChocolateyPackage 'ext2fsd' 'exe' '' 'http://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.51/Ext2Fsd-0.51.exe'
--- ext2fsd/tools/chocolateyUninstall.ps1
+++ ext2fsd/tools/chocolateyUninstall.ps1
@@ -1,15 +1,12 @@
-$os_name = (Get-WmiObject -class Win32_OperatingSystem).Caption
-if ($os_name -like '* 8*'){
-Remove-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$env:temp\chocolatey\ext2fsd\ext2fsdInstall.exe"
-}
+$packageName = "ext2fsd"
+$fileType = "exe"
+$silentArgs = "/VERYSILENT /NORESTART"
+$unpath = "${Env:ProgramFiles}\Ext2Fsd\unins000.exe"
+$unpathx86 = "${Env:ProgramFiles(x86)}\Ext2Fsd\unins000.exe"
+$validExitCodes = @(0,2)
 
-$unfile = "Ext2Fsd\unins000.exe"
+if (Test-Path $unpath) {$file = "$unpath"}
+if (Test-Path $unpathx86) {$file = "$unpathx86"}
 
-if (Test-Path "${Env:ProgramFiles(x86)}\$unfile") {
-    $unpath = "${Env:ProgramFiles(x86)}\$unfile"
-}
-else {
-    $unpath = "${Env:ProgramFiles}\$unfile"
-}
-Uninstall-ChocolateyPackage 'ext2fsd' 'exe' '' "$unpath"
+Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file  -validExitCodes $validExitCodes
 Write-Host The system must be rebooted for the changes to be completed.
