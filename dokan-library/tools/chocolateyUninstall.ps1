8ab3385ed402498f90a4ed0996e58a17ec32da28
TomOne <user.email@fake.com>
2013-05-19 19:26:19 +0200

    compatibility mode for Windows 8 and higher

--- dokan-library/tools/chocolateyUninstall.ps1
+++ dokan-library/tools/chocolateyUninstall.ps1
@@ -1,9 +1,12 @@
-$unfile = "Dokan\DokanLibrary\DokanUninstall.exe"
+$packageName = "dokan-library"
+$fileType = "exe"
+$silentArgs = "/S"
+$unpath = "${Env:ProgramFiles}\Dokan\DokanLibrary\DokanUninstall.exe"
+$unpathx86 = "${Env:ProgramFiles(x86)}\Dokan\DokanLibrary\DokanUninstall.exe"
+$validExitCodes = @(0)
 
-if (Test-Path "${Env:ProgramFiles(x86)}\$unfile") {
-    $unpath = "${Env:ProgramFiles(x86)}\$unfile"
-}
-else {
-    $unpath = "${Env:ProgramFiles}\$unfile"
-}
-Uninstall-ChocolateyPackage 'dokan-library' 'exe' '/S' "$unpath"
+if (Test-Path $unpath) {$file = "$unpath"}
+if (Test-Path $unpathx86) {$file = "$unpathx86"}
+
+Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file  -validExitCodes $validExitCodes
+Write-Host The system must be rebooted for the changes to be completed.
