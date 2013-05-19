896f53bc49b64b873ee498c7304d45448d4f832a
TomOne <user.email@fake.com>
2013-05-20 01:37:42 +0200

    added haali-media-splitter

---
+++ haali-media-splitter/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$packageName = "haali-media-splitter"
+$fileType = "exe"
+$silentArgs = "/S"
+$url = "http://haali.su/mkv/MatroskaSplitter.exe"
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
