3e7fed1fd429af9b6a6f63ea678795cefb62663a
TomOne <user.email@fake.com>
2013-05-20 01:19:37 +0200

    added vp8-vfw

---
+++ vp8-vfw/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+try {
+
+    $packageName = "vp8-vfw"
+    $fileType = "exe"
+    $silentArgs = "/VERYSILENT"
+    $validExitCodes = @(0)
+    $file = "$env:windir\unins000.exe"
+
+    if (Test-Path "$file") {
+        Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file -validExitCodes $validExitCodes
+    }
+    
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
