60a9ae388486b0b1144532b921c1173227fd216b
TomOne <user.email@fake.com>
2013-05-26 15:34:57 +0200

    added codeblocks

---
+++ codeblocks/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = "codeblocks"
+$fileType = "exe"
+$silentArgs = "/S"
+$url = "http://sourceforge.net/projects/codeblocks/files/Binaries/12.11/Windows/codeblocks-12.11mingw-setup.exe/download"
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
