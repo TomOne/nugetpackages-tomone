8379a7b787a805cea2964d205b283da3327b0418
TomOne <user.email@fake.com>
2013-05-29 14:01:45 +0200

    update tightvnc to 2.7.3

--- tightvnc/tools/chocolateyInstall.ps1
+++ tightvnc/tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-Install-ChocolateyPackage 'tightvnc' 'msi' '/quiet /norestart' 'http://www.tightvnc.com/download/2.7.1/tightvnc-2.7.1-setup-32bit.msi' 'http://www.tightvnc.com/download/2.7.1/tightvnc-2.7.1-setup-64bit.msi'
+﻿$packageName = "tightvnc"
+$fileType = "msi"
+$silentArgs = "/quiet /norestart"
+$url = "http://www.tightvnc.com/download/2.7.3/tightvnc-2.7.3-setup-32bit.msi"
+$url64bit = "http://www.tightvnc.com/download/2.7.3/tightvnc-2.7.3-setup-64bit.msi"
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
