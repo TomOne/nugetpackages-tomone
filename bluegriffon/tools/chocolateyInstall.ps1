e9c49bf65172e867f5ab1f7f7e3cd0c64ff7b308
TomOne <user.email@fake.com>
2013-05-30 01:14:04 +0200

    added bluegriffon

---
+++ bluegriffon/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = "bluegriffon"
+$fileType = "exe"
+$silentArgs = "/VERYSILENT"
+$url = "http://bluegriffon.org/freshmeat/1.7/bluegriffon-1.7.exe"
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
