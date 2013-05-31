a58ced030d15d7adc9e233e16ecde05128ec4a78
TomOne <user.email@fake.com>
2013-05-31 13:38:20 +0200

    added xnview v2.03, updated icon, use full version

---
+++ xnview/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = "xnview"
+$fileType = "exe"
+$silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+$url = "http://download3.xnview.com/XnView-win-full.exe"
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
