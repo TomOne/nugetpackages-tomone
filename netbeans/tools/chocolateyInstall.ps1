b307ba0e155afbfc6ab9dc8100efcd99b7f3f497
TomOne <user.email@fake.com>
2013-06-03 16:14:16 +0200

    cleanup

---
+++ netbeans/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿try {
+
+    $packageName = "netbeans"
+    $fileType = "exe"
+    $silentArgs = "--silent"
+    $language = (Get-Culture).Name # Get language and country code separated by hyphen
+    $language = "$language" -replace '-[a-z]{2}', '' # Remove hyphen and country code
+
+    if ($language -eq "en") { # Installer for English
+        $url = "http://dlc.sun.com.edgesuite.net/netbeans/7.3/final/bundles/netbeans-7.3-windows.exe"
+        Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+    } else { # Installer for all other languages
+        $url = "http://bits.netbeans.org/netbeans/7.3/community/latest/bundles/netbeans-7.3-windows.exe"
+        Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+    }
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
---
+++ regshot/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿try { 
+  
+  $packageName = "RegShot"
+  $url = "http://sourceforge.net/projects/regshot/files/regshot/1.9.0/Regshot-1.9.0.7z/download"
+
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+
+  $mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $contentDir = ($mydir | Split-Path | Join-Path -ChildPath "content")
+  $binDir = ($mydir | Split-Path | Join-Path -ChildPath "bin")
+  $toolsDir = ($mydir | Split-Path | Join-Path -ChildPath "tools")
+  if($is64bit) {
+  	$shortcutTarget = Join-Path $binDir "${packageName}_x64.exe"
+  } else {
+  	$shortcutTarget = Join-Path $binDir "${packageName}.exe"
+  }
+  
+  Get-ChocolateyWebFile $packageName $fileFullPath $url
+
+  Install-ChocolateyDesktopLink $shortcutTarget
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
