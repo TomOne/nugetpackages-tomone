d6817c1d2abb8f06e92e5c0f52acc78e9b4c7db5
TomOne <user.email@fake.com>
2013-06-10 23:39:10 +0200

    quicktime now install apple application support too to work correctly

--- quicktime/tools/chocolateyInstall.ps1
+++ quicktime/tools/chocolateyInstall.ps1
@@ -1,1 +1,22 @@
-﻿Install-ChocolateyPackage 'Quicktime' 'exe' '/quiet' 'http://appldnld.apple.com/QuickTime/041-9648.20130522.Rftg5/QuickTimeInstaller.exe'
+﻿$packageName = 'quicktime installer to extract'
+$url = 'http://appldnld.apple.com/QuickTime/041-9648.20130522.Rftg5/QuickTimeInstaller.exe'
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$filePath = "$env:TEMP\chocolatey\$packageName"
+$fileFullPath = "$filePath\$packageName`Install.exe"
+
+if (-not (Test-Path $filePath)) {
+    New-Item -ItemType directory -Path $filePath
+}
+
+Get-ChocolateyWebFile $packageName $fileFullPath $url
+
+Start-Process "7za" -ArgumentList "x -o`"$filePath`" -y `"$fileFullPath`"" -Wait
+
+$packageName = 'appleapplicationsupport'
+$file = "$filePath\AppleApplicationSupport.msi"
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file
+
+$packageName = 'quicktime'
+$file = "$filePath\QuickTime.msi"
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file
