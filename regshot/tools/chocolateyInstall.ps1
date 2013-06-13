47904609d7cb782129c8f951f90d2ddc57eb1957
TomOne <user.email@fake.com>
2013-06-13 21:59:52 +0200

    added regshot and bumped to v1.9.0

--- regshot/tools/chocolateyInstall.ps1
+++ regshot/tools/chocolateyInstall.ps1
@@ -1,22 +1,21 @@
 ﻿try { 
   
-  $packageName = "RegShot"
-  $url = "http://sourceforge.net/projects/regshot/files/regshot/1.9.0/Regshot-1.9.0.7z/download"
+  $packageName = 'RegShot'
+  $scriptDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $url = 'http://sourceforge.net/projects/regshot/files/regshot/1.9.0/Regshot-1.9.0.7z/download'
 
   $processor = Get-WmiObject Win32_Processor
   $is64bit = $processor.AddressWidth -eq 64
-
-  $mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
-  $contentDir = ($mydir | Split-Path | Join-Path -ChildPath "content")
-  $binDir = ($mydir | Split-Path | Join-Path -ChildPath "bin")
-  $toolsDir = ($mydir | Split-Path | Join-Path -ChildPath "tools")
   if($is64bit) {
-  	$shortcutTarget = Join-Path $binDir "${packageName}_x64.exe"
+  	$shortcutTarget = "$scriptDir\Regshot-x64-Unicode.exe"
   } else {
-  	$shortcutTarget = Join-Path $binDir "${packageName}.exe"
+  	$shortcutTarget = "$scriptDir\Regshot-x86-Unicode.exe"
   }
-  
+
+  $fileFullPath = "$env:TEMP\chocolatey\$packageName\$packageName`Install.7z"
+
   Get-ChocolateyWebFile $packageName $fileFullPath $url
+  Start-Process "7za" -ArgumentList "x -o`"$scriptDir`" -y `"$fileFullPath`"" -Wait
 
   Install-ChocolateyDesktopLink $shortcutTarget
 
