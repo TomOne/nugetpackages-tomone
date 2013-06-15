a0e3cdb2989f0605ad571a743f6a05fefd610195
TomOne <user.email@fake.com>
2013-06-15 14:19:00 +0200

    bumped nomacs to v1.2.0

--- nomacs/tools/chocolateyInstall.ps1
+++ nomacs/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-# If Windows 2000/XP, download matching version
+﻿# If Windows 2000/XP, download matching version
 $WinVersion = [System.Environment]::OSVersion.Version.Major
 if ($WinVersion -eq 5) {
-Install-ChocolateyPackage 'nomacs' 'exe' '/S' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.0.2/nomacs-setup-winXP-x86.exe/download'
+Install-ChocolateyPackage 'nomacs' 'exe' '/S' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.2.0/nomacs-setup-1.2.0-WinXP-x86.exe/download'
 }
 
 
 # If Windows Vista/7/8 or later, download matching version
 $WinVersion = [System.Environment]::OSVersion.Version.Major
 if ($WinVersion -ge 6) {
-Install-ChocolateyPackage 'nomacs' 'exe' '/S' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.0.2/nomacs-setup.exe/download' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.0.2/nomacs-setup-x64.exe/download'
+Install-ChocolateyPackage 'nomacs' 'exe' '/S' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.2.0/nomacs-setup-1.2.0-x86.exe/download' 'http://sourceforge.net/projects/nomacs/files/nomacs-1.2.0/nomacs-setup-1.2.0-x64.exe/download'
 }
