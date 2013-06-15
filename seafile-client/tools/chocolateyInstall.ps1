6ea37becb5c7790c718b5d4fb9b30a57c6e20baf
TomOne <user.email@fake.com>
2013-06-15 14:31:32 +0200

    bumped seafile-client to v1.7.1

--- seafile-client/tools/chocolateyInstall.ps1
+++ seafile-client/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = "seafile-client"
-$fileType = "msi"
-$silentArgs = "/passive"
-$url = "http://seafile.googlecode.com/files/seafile-1.6.1-en.msi"
+﻿$packageName = 'seafile-client'
+$fileType = 'msi'
+$silentArgs = '/passive'
+$url = 'http://seafile.googlecode.com/files/seafile-1.7.1-en.msi'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
