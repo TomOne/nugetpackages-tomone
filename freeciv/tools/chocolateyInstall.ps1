f94d3bea7df2d952ffaecb910d986920ab85d0c2
TomOne <user.email@fake.com>
2013-06-16 00:55:28 +0200

    added freeciv

---
+++ freeciv/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'freeciv'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'http://sourceforge.net/projects/freeciv/files/Freeciv%202.3/2.3.4/Freeciv-2.3.4-win32-gtk2-setup.exe/download'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
