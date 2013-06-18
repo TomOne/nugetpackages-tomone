25c4fd7f49c4afdc5ab0122bd4118128fda11ce8
TomOne <user.email@fake.com>
2013-06-18 20:56:06 +0200

    bumped clipgrab to 3.2.0.13

--- clipgrab/tools/chocolateyInstall.ps1
+++ clipgrab/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = "clipgrab"
-$fileType = "exe"
-$silentArgs = "/VERYSILENT"
-$url = "http://clipgrab.de/download/clipgrab-3.2.0.11.exe"
+﻿$packageName = 'clipgrab'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT'
+$url = 'http://clipgrab.de/download/clipgrab-3.2.0.13.exe'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
