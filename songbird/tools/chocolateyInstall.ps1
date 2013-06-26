f87c7eed08c8f9f568116733de83d0fb142d198c
TomOne <user.email@fake.com>
2013-06-26 22:06:03 +0200

    fixed songbird package

--- songbird/tools/chocolateyInstall.ps1
+++ songbird/tools/chocolateyInstall.ps1
@@ -1,27 +1,22 @@
-﻿$packageName = "songbird"
-$fileType = "exe"
-$url = "http://www.getsongbird.com/desktop/index.php?download=GSB_windows"
-$unzipLocation = "$env:TEMP"
-$silentArgs = "/S"
-$file = "$env:TEMP\Songbird_2.2.0-2453_windows-i686-msvc8.exe"
-$ignoreFile = "$file.ignore"
-
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
-Remove-Item "$env:TEMP\`_`_MACOSX" -Recurse
+﻿$packageName = 'songbird'
+$fileType = 'exe'
+$url = 'http://www.getsongbird.com/desktop/index.php?download=GSB_windows'
+$silentArgs = '/S'
 
 # Set compatibility mode to Windows 7 if operating system is Windows 8 or higher.
 $WindowsVersion = (Get-WmiObject -class Win32_OperatingSystem).Version
-if ($WindowsVersion -ge "6.2.9200") {
-    if (-not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags" -Name Layers 
-    }
-    New-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -propertyType String -Name "$file" -value "~ WIN7RTM"
-    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file
+if ($WindowsVersion -ge '6.2.9200') {
+
+    $regDir = 'HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers'
+    $file = "$env:TEMP\chocolatey\$packageName\${packageName}Install.exe"
+
+    if (-not (Test-Path $regDir)) {New-Item $regDir}
+    New-ItemProperty -Path $regDir -PropertyType String -Name $file -Value '~ WIN7RTM' -Force
+
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
     # Delete compatibility mode for installer, because it’s not needed anymore
-    Remove-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$file"
-} else {
-    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file
+    Remove-ItemProperty -Path $regDir -Name $file
 }
 
-Remove-Item $file
-Remove-Item $ignoreFile
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
