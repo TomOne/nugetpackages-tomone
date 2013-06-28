d8047bfa9439d3a8330a53e3dbdf195ba94c3c73
TomOne <user.email@fake.com>
2013-06-28 19:35:22 +0200

    now the imagemagick packages use regex to obtain the download url to prevent 404 errors

--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,11 +1,22 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.6-2-Q16-x86-windows.zip'
 
-try { 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-  Install-ChocolateyZipPackage $packageName $url $installDir
-  Write-ChocolateySuccess $packageName
+try {
+    $tempDir = "$env:TEMP\chocolatey\$packageName"
+    if (-not (Test-Path $tempDir)) {New-Item $tempDir -ItemType directory}
+
+    $listUrl = 'http://www.imagemagick.org/download/binaries/'
+    $fileFullPath = "$tempDir\temp.html"
+
+    # Obtain the download URL of the latest version using regex
+    Get-ChocolateyWebFile 'HTML file containing the URL' $fileFullPath $listUrl
+    $content = Select-String -Path $fileFullPath -Pattern 'zip'
+    $url = $content -replace '.+(ImageMagick-)([\d-\.]{1,})(-Q16-x86-windows.zip).+', 'http://www.imagemagick.org/download/binaries/$1$2$3'
+    $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+    Install-ChocolateyZipPackage $packageName $url $installDir
+
+    Write-ChocolateySuccess $packageName
 } catch {
-  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-  throw 
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw 
 }
