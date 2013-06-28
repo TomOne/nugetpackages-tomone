13b05d186dcf72567b3783e1df64ab2c9eeea741
TomOne <user.email@fake.com>
2013-06-28 15:54:59 +0200

    bumped imagemagick to new version

---
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'imagemagick.tool'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.6-2-Q16-x86-windows.zip'
+
+try { 
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  Install-ChocolateyZipPackage $packageName $url $installDir
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+  throw 
+}
