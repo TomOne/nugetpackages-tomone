5b84bde42ea1d69869becf6a03225fb1d56eee92
TomOne <user.email@fake.com>
2013-07-01 17:55:16 +0200

    add checkifinstalled-script and firefox-profile

---
+++ checkifinstalled-chocolatey/checkifinstalled.ps1
@@ -0,0 +1,22 @@
+﻿Function checkIfPackageVersionAlreadyInstalled ($programUninstallEntryName, $checkVersion) {
+    # For every program on 32-bit-systems and for 64-bit-software on 64-bit systems
+    # $programUninstallEntryName = The value which contains the registry-key “DisplayName” of the affected program
+    $registrypath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    $installedVersion = (Get-ItemProperty $registrypath | Select DisplayName, DisplayVersion | Where-Object {$_.DisplayName -like "*$programUninstallEntryName*"}).DisplayVersion
+    # $installedVersion is the value of the registry-key “DisplayVersion” of the affected program
+
+    if ($installedVersion -eq $null) {
+        # For 32-bit-software on 64-bit systems
+        $registrypath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        $installedVersion = (Get-ItemProperty $registrypath | Select DisplayName, DisplayVersion | Where-Object {$_.DisplayName -like "*$programUninstallEntryName*"}).DisplayVersion
+    }
+
+    if ($installedVersion -ge $checkVersion) {
+        $alreadyInstalled = "True"
+    } else {$alreadyInstalled = "False"}
+    return $alreadyInstalled
+}
+
+
+#checkIfPackageVersionAlreadyInstalled "CCleaner" "4.1"
+checkIfPackageVersionAlreadyInstalled "CDBurnerXP" "4.5.1.4003"
---
+++ firefox-sicheres-profil/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿try {
+
+	$name = "firefox-sicheres-profil"
+	$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$fileFullPath = "$scriptPath\firefox_sicheres_profil.zip"
+    
+    if (Test-Path $env:APPDATA\Mozilla\sicheres_profil_installiert) {
+        Write-Host Das sichere Profil ist bereits installiert. Deshalb wird keine weitere Aktion vorgenommen.
+    } elseif (Test-Path $env:APPDATA\Mozilla) {
+        
+        $i = 1
+        while () {}
+        
+        Rename-Item -NewName Mozilla.old -Path $env:APPDATA\Mozilla
+        Get-ChocolateyUnzip $fileFullPath $env:APPDATA
+    } else {
+        Get-ChocolateyUnzip $fileFullPath $env:APPDATA
+    }
+
+    Write-ChocolateySuccess $name
+} catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw
+}
